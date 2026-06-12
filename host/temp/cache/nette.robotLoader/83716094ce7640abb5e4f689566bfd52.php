958abea63acd98caf604bcdc7c12a9d4a58dc3d9
vgalitsky <concept.galitsky@gmail.com>
2026-06-12 10:49:57 -0400

    nette fresh app

---
+++ host/temp/cache/nette.robotLoader/83716094ce7640abb5e4f689566bfd52.php
@@ -0,0 +1,46 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Core\\RouterFactory' => 
+    array (
+      0 => '/var/www/app/Core/RouterFactory.php',
+      1 => 1781264188,
+    ),
+    'App\\Bootstrap' => 
+    array (
+      0 => '/var/www/app/Bootstrap.php',
+      1 => 1781100913,
+    ),
+    'App\\Presentation\\Home\\HomePresenter' => 
+    array (
+      0 => '/var/www/app/Presentation/Home/HomePresenter.php',
+      1 => 1780339767,
+    ),
+    'App\\Presentation\\Error\\Error5xx\\Error5xxPresenter' => 
+    array (
+      0 => '/var/www/app/Presentation/Error/Error5xx/Error5xxPresenter.php',
+      1 => 1780339767,
+    ),
+    'App\\Presentation\\Error\\Error4xx\\Error4xxPresenter' => 
+    array (
+      0 => '/var/www/app/Presentation/Error/Error4xx/Error4xxPresenter.php',
+      1 => 1780339767,
+    ),
+    'App\\Presentation\\Accessory\\LatteExtension' => 
+    array (
+      0 => '/var/www/app/Presentation/Accessory/LatteExtension.php',
+      1 => 1780339767,
+    ),
+  ),
+  1 => 
+  array (
+    'null' => 3,
+    'Mtr\\MiniCRM\\Presentation\\Customers\\CustomersIndexTemplate' => 3,
+    'Mtr\\MiniCRM\\Presentation\\Customers\\CustomersTemplate' => 3,
+    'App\\Presentation\\Customers\\CustomersPresenter' => 3,
+  ),
+  2 => 
+  array (
+  ),
+);
